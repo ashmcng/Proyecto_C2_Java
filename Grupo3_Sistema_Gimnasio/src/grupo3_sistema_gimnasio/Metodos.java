577b02d47548c386612b943bc742249c0cbd1aa1
Ashleyy <ashleymcnish67@gmail.com>
2025-08-12 17:09:29 -0600

    update parqueo

--- Grupo3_Sistema_Gimnasio/src/grupo3_sistema_gimnasio/Metodos.java
+++ Grupo3_Sistema_Gimnasio/src/grupo3_sistema_gimnasio/Metodos.java
@@ -8,8 +8,6 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import javax.swing.JOptionPane;
 
